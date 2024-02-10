0c0c6a713850c560c361986212acbcb8fb7ac848
greenturtle537 <greenturtle537@mail.com>
2024-02-10 17:34:11 -0500

    add gitignore for local ref docs

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 import * as THREE from 'three';
 import WebGL from 'three/addons/capabilities/WebGL.js';
+import { FirstPersonControls } from 'three/addons/controls/FirstPersonControls.js';
+
 
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+//const controls = new FirstPersonControls(camera, renderer.domElement);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
