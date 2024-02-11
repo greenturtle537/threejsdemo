cac535410051d1f63c9d328ef737abfca9cc30e3
greenturtle537 <greenturtle537@mail.com>
2024-02-11 16:19:43 -0500

    migration to class based logic

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ import * as THREE from 'three';
 import WebGL from 'three/addons/capabilities/WebGL.js';
 import { FirstPersonControls } from 'three/addons/controls/FirstPersonControls.js';
 import Stats from 'three/addons/libs/stats.module.js';
-
-let scene, controls, renderer, camera, clock, stats;
+//todo cleanup of global vars
+let renderer, clock, stats;
 let geometry, cube, plane;
 let posdebug = document.getElementById("posdebug")
 
@@ -22,8 +22,8 @@ class threejsdemo {
 	init() {
 		posdebug.innerHTML = "x:0 y:0 z:0"
 
-		scene = new THREE.Scene();
-		camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 1000 );
+		this.scene = new THREE.Scene();
+		this.camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 1000 );
 
 		clock = new THREE.Clock();
 
@@ -36,29 +36,29 @@ class threejsdemo {
 		
 		document.body.appendChild( stats.dom );
 
-		controls = new FirstPersonControls( camera, renderer.domElement );
-		controls.movementSpeed = 1;
-		controls.domElement = renderer.domElement;
-		controls.autoForward = false;
-		controls.dragToLook = false;
-		controls.activeLook = false;
-		controls.lookAt(0, 0, 0);
+		this.controls = new FirstPersonControls( this.camera, renderer.domElement );
+		this.controls.movementSpeed = 1;
+		this.controls.domElement = renderer.domElement;
+		this.controls.autoForward = false;
+		this.controls.dragToLook = false;
+		this.controls.activeLook = false;
+		this.controls.lookAt(0, 0, 0);
 
 		geometry = new THREE.BoxGeometry( 3, 3, 3 );
 		const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 		cube = new THREE.Mesh( geometry, material );
-		scene.add( cube );
+		this.scene.add( cube );
 
 		geometry = new THREE.PlaneGeometry( 10, 10 );
 		const material2 = new THREE.MeshBasicMaterial( {color: 0xffffff, side: THREE.DoubleSide} );
 		plane = new THREE.Mesh( geometry, material2 );
 		plane.rotation.x = 90;
 		plane.position.y = 0;
-		scene.add( plane );
+		this.scene.add( plane );
 
-		camera.position.z = 5;
-		camera.position.x = 0;
-		camera.position.y = -1;
+		this.camera.position.z = 5;
+		this.camera.position.x = 0;
+		this.camera.position.y = -1;
 	}
 	posupdate(x=0,y=0,z=0) {
 		posdebug.innerHTML = "x:"+x.toString()+" y:"+y.toString()+" z:"+z.toString();
@@ -68,15 +68,15 @@ class threejsdemo {
 		
 		cube.rotation.x += 0.01;
 		cube.rotation.y += 0.01;
-		renderer.render( scene, camera );
+		renderer.render( this.scene, this.camera );
 	
 		this.render();
 		stats.update();
-		this.posupdate(camera.position.x,camera.position.y,camera.position.z)
+		this.posupdate(this.camera.position.x,this.camera.position.y,this.camera.position.z)
 	}
 	render() {
 		const delta = clock.getDelta();
-		controls.update( delta );
+		this.controls.update( delta );
 	}
 }
 
@@ -132,16 +132,16 @@ function animate() {
 	
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
-	renderer.render( scene, camera );
+	renderer.render( _APP.scene, _APP.camera );
 
 	render();
 	stats.update();
-	posupdate(camera.position.x,camera.position.y,camera.position.z)
+	posupdate(_APP.camera.position.x,_APP.camera.position.y,_APP.camera.position.z)
 }
 
 function render() {
 	const delta = clock.getDelta();
-	controls.update( delta );
+	_APP.controls.update( delta );
 }
 
 let _APP = null;
